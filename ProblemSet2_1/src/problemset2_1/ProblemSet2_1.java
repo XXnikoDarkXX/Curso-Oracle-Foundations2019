1882497eadc25300945603f66a2c11cc18e67f20
XXnikoDarkXX <nicocheto1@gmail.com>
2020-06-17 19:49:39 +0200

    Curso

---
+++ ProblemSet2_1/src/problemset2_1/ProblemSet2_1.java
@@ -0,0 +1,24 @@
+package problemset2_1;
+
+public class ProblemSet2_1 {
+    public static void main(String[] args) {
+        //Use 8 print statements to print a smiley face.
+        //Your art will rely on only a single character, besides space, such as X or #.
+        //Utilice ocho sentencias print para volver a crear la cara sonriente anterior. Su arte se basará 
+        //en un solo carácter, además del espacio, como X o #.
+        
+        System.out.println("  ####  ");
+        System.out.println(" #    # ");
+        System.out.println("# #  # #");
+        System.out.println("#      #");
+        System.out.println("# #  # #");
+        System.out.println("#  ##  #");
+        System.out.println(" #    # ");
+        System.out.println("  ####  ");
+        
+        
+        
+        
+        
+    }   
+}
---
+++ Text03/src/text03/Text03.java
@@ -0,0 +1,24 @@
+package text03;
+
+public class Text03 {
+    /**
+     * Metodo para imprimir pon pantalla las patas del gato
+     */
+    public static void drawLegs(){
+        System.out.println("     ||     ||      ");
+        System.out.println("     ||     ||      ");
+        System.out.println("    (||)   (||)     ");
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("   /\\         /\\   ");
+        System.out.println("  /  \\_______/  \\  ");
+        System.out.println(" /               \\ ");
+        System.out.println("(  /\\         /\\  )");
+        System.out.println("====     V     ====");
+        System.out.println("======(__|__)======");
+        System.out.println("  (             )  ");
+        System.out.println("   (___________)   ");
+        drawLegs();
+    }    
+}
