7b098753e6227e25f3020589953d7b31c948826e
XXnikoDarkXX <nicocheto1@gmail.com>
2020-06-25 19:04:23 +0200

    curso

--- Input01/src/input01/Input01.java
+++ Input01/src/input01/Input01.java
@@ -1,17 +1,17 @@
 package input01;
 
+import javax.swing.JOptionPane;
+
 public class Input01 {
     public static void main(String[] args) {
         //Create a JOptionPane.
         //Store the input as a String and print it.
         
-        
-        
+        String input = JOptionPane.showInputDialog("Escribe un mensaje");
+        System.out.println(input);
         //Parse the input as an int.
         //Print its value +1
-        
-        
-        
+       
         //Try creating a dialog, parsing it, and initializing an int in a single line.
         //You should have only one semicolon (;) in this line.
 
---
+++ Input02/src/input02/Input02.java
@@ -0,0 +1,13 @@
+package input02;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class Input02 {
+    public static void main(String[] args) {
+        
+      Scanner sc=new Scanner (System.in);
+      sc.close();
+        System.out.println(sc.nextInt());
+    }
+}
