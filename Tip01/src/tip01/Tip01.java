c4dad71b94d3844c32eedb66b204ca1ccd98fc6f
XXnikoDarkXX <nicocheto1@gmail.com>
2020-10-01 21:48:40 +0200

    curso oracle

--- Tip01/src/tip01/Tip01.java
+++ Tip01/src/tip01/Tip01.java
@@ -25,13 +25,26 @@ public class Tip01 {
         persona1=persona1/100;
         totalPersona1+=persona1+10;
         System.out.println(totalPersona1);
-        
+        double persona2=calcularTotal((byte)12,(byte)5,(byte)15);//Total importe persona2
+        System.out.println(persona2);
+         double persona3=calcularTotal((byte)9,(byte)5,(byte)15);//Total importe persona2
+          System.out.println(persona3);
+           double persona4=calcularTotal((byte)8,(byte)5,(byte)15);//Total importe persona2
+          System.out.println(persona4);
+           double persona5=calcularTotal((byte)7,(byte)5,(byte)15);//Total importe persona2
+          System.out.println(persona5);
+           double persona6=calcularTotal((byte)15,(byte)5,(byte)15);//Total importe persona2
+          System.out.println(persona2);
+           double persona7=calcularTotal((byte)11,(byte)5,(byte)15);//Total importe persona2
+          System.out.println(persona7);
+           double persona8=calcularTotal((byte)30,(byte)5,(byte)15);//Total importe persona2
+          System.out.println(persona8);
     }
     /**
      * 
      * @param importeInicio el importe inicial
-     * @param impuestos
-     * @param propina
+     * @param impuestos impuesto que son al 5% del importeInicio
+     * @param propina propina que son al 15% del importeInicio
      * @return 
      */
     public static double calcularTotal(byte importeInicio,byte impuestos,byte propina){
@@ -41,8 +54,10 @@ public class Tip01 {
         semiTotal=semiTotal/100;
         total=semiTotal;
         semiTotal=importeInicio*propina;
+        semiTotal=semiTotal/100;
+        total+=semiTotal;
         
-        
+        return total+importeInicio;
         
     }
 }
